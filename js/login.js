e5d70bbea3a9bfe736fbd96ef18f418824e6ef5f
Sopaa <pierre.bourgeois@hetic.net>
2017-12-18 15:07:50 +0100

    Start Index and create homepage.htlm

---
+++ js/login.js
@@ -0,0 +1,6 @@
+var logButton = document.querySelector('.loginButton');
+
+logButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location = "../homepage.html";
+});
