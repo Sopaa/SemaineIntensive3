5a123abbb6a42fd60f619f635a359473561151b2
Sopaa <pierre.bourgeois@hetic.net>
2017-12-19 07:54:42 +0100

    add css for button 'se connecter // s'enregistrer

--- js/login.js
+++ js/login.js
@@ -4,3 +4,24 @@ logButton.addEventListener("click", function(event) {
   event.preventDefault();
   window.location = "homepage.html";
 });
+
+
+
+var bckgndlog = document.querySelector('.login');
+var logblock = document.querySelector('.loginGroupIn');
+var bckgndreg = document.querySelector('.register');
+var regblock = document.querySelector('.registerGroupIn');
+
+bckgndlog.addEventListener("click", function () {
+  bckgndlog.style.backgroundColor = "#FCFAFA";
+  logblock.style.display = "block";
+  bckgndreg.style.backgroundColor = "#E6E4E4";
+  regblock.style.display = "none";
+})
+
+bckgndreg.addEventListener("click", function () {
+  bckgndlog.style.backgroundColor = "#E6E4E4";
+  logblock.style.display = "none";
+  bckgndreg.style.backgroundColor = "#FCFAFA";
+  regblock.style.display = "block";
+})
